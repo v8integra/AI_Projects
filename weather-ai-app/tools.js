62298d07a6f45b84b9dd4d248334c2d4d196349a
unknown <keith.francis83@gmail.com>
2026-03-31 14:55:04 -0600

    Setup OpenAI functions agent

--- weather-ai-app/tools.js
+++ weather-ai-app/tools.js
@@ -1,13 +1,41 @@
-export async function getCurrentWeather(){
-  const weather = {
-    temperature: '72',
-    unit: 'F',
-    forecast: 'sunny'
-  }
-
-  return JSON.stringify(weather)
+export async function getCurrentWeather({ location }) {
+    const weather = {
+        location,
+        temperature: "75",
+        forecast: "sunny"
+    }
+    return JSON.stringify(weather)
 }
 
 export async function getLocation() {
-  return 'Denver, CO'
+    try {
+        const response = await fetch('https://ipapi.co/json/')
+        const text = await response.json()
+        return JSON.stringify(text)
+    } catch (err) {
+        console.log(err)
+    }
 }
+
+export const functions = [
+    {
+        function: getCurrentWeather,
+        parameters: {
+            type: "object",
+            properties: {
+                location: {
+                    type: "string",
+                    description: "The location from where to get the weather"
+                }
+            },
+            required: ["location"]
+        }
+    },
+    {
+        function: getLocation,
+        parameters: {
+            type: "object",
+            properties: {}
+        }
+    },
+]
